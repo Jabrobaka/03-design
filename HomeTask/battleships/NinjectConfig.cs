1ae0bcac55880bed434e93716810af6ef7c66e94
Jabrobaka <alexander.kulikov.ru@gmail.com>
2015-02-23 21:41:42 +0500

    Добавил модуль конфигурирования Ninject

---
+++ HomeTask/battleships/NinjectConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ninject;
+using Ninject.Modules;
+
+namespace battleships
+{
+    class NinjectConfig : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<Settings>().ToSelf().InSingletonScope().WithConstructorArgument("settings.txt");
+            var settings = Kernel.Get<Settings>();
+
+            Bind<IGameVisualizer>().To<GameVisualizer>();
+            Bind<IMapGenerator>().To<MapGenerator>()
+                .WithConstructorArgument("settings", settings)
+                .WithConstructorArgument("random", new Random(settings.RandomSeed));
+            Bind<IProcessMonitor>().To<ProcessMonitor>()
+                .WithConstructorArgument("timeLimit", TimeSpan.FromSeconds(settings.TimeLimitSeconds * settings.GamesCount))
+                .WithConstructorArgument("memoryLimit", (long) settings.MemoryLimit);
+            Bind<IAiFactory>().To<AiFactory>();
+            Bind<IGameFactory>().To<GameFactory>();
+        }
+    }
+}
