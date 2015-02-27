71d26abe39aa3f6d9b03023bc3f378f6ac5523ba
Jabrobaka <alexander.kulikov.ru@gmail.com>
2015-02-28 00:45:09 +0500

    Забыл сохранить файл перед предыдущим коммитом Теперь метод есть

--- HomeTask/battleships/Program.cs
+++ HomeTask/battleships/Program.cs
@@ -20,31 +20,36 @@ namespace battleships
 			var settings = new Settings("settings.txt");
 		    if (File.Exists(aiPath))
 		    {
-
-                var tester = new AiTester(settings);
-                var aiFactory = new AiFactory(settings);
-                var visualiser = new GameVisualizer();
-		        var ai = aiFactory.CreateAi(aiPath);
-		        var gameGenerator = new GameGenerator(settings);
-                gameGenerator.AddGameStepPerformedHandler(visualiser.Visualize);
-		        if (settings.Verbose)
-		        {
-		            gameGenerator.AddGameEndedHandler(new GameVerboseWriter().Write);
-		        }
-		        if (settings.Interactive)
-		        {
-                    gameGenerator.AddAiCrashHandler(game =>
-                    {
-                        Console.WriteLine(game.LastError.Message);
-                        Console.ReadKey();
-                    });
-		        }
-		        var testReporter = new AiTestReporter(settings);
-		        var testResult = tester.TestSingleAi(ai, gameGenerator);
-                testReporter.WriteTotal(testResult);
+                var testReporter = new AiTestReporter(settings);
+                var testResult = TestAi(settings, aiPath);
+		        testReporter.WriteTotal(testResult);
 		    }
 		    else
 		        Console.WriteLine("No AI exe-file " + aiPath);
 		}
+
+	    private static AiTestResult TestAi(Settings settings, string aiPath)
+	    {
+	        var tester = new AiTester(settings);
+	        var aiFactory = new AiFactory(settings);
+	        var visualiser = new GameVisualizer();
+	        var ai = aiFactory.CreateAi(aiPath);
+	        var gameGenerator = new GameGenerator(settings);
+	        gameGenerator.AddGameStepPerformedHandler(visualiser.Visualize);
+	        if (settings.Verbose)
+	        {
+	            gameGenerator.AddGameEndedHandler(new GameVerboseWriter().Write);
+	        }
+	        if (settings.Interactive)
+	        {
+	            gameGenerator.AddAiCrashHandler(game =>
+	            {
+	                Console.WriteLine(game.LastError.Message);
+	                Console.ReadKey();
+	            });
+	        }
+	        var testResult = tester.TestSingleAi(ai, gameGenerator);
+	        return testResult;
+	    }
 	}
 }
