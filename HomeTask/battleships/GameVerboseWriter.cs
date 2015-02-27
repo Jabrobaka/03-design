c9d260f357d5cc82530a2dfabde0e20ef287db06
Jabrobaka <alexander.kulikov.ru@gmail.com>
2015-02-28 00:37:54 +0500

    Выделил подробное описание игры (settings.verbose) в отдельный класс, метод которого обрабатывает событие GameEnded

---
+++ HomeTask/battleships/GameVerboseWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace battleships
+{
+    class GameVerboseWriter
+    {
+        private int gamesWritten = 0;
+        public void Write(Game game)
+        {
+            Console.WriteLine(
+                "Game #{3,4}: Turns {0,4}, BadShots {1}{2}",
+                game.TurnsCount, game.BadShots, game.AiCrashed ? ", Crashed" : "", gamesWritten++);
+        }
+    }
+}
